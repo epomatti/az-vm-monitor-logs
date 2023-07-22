ab698687453060dbb209ae7c7b8e53c8c2ae88d4
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-07-22 14:44:49 -0300

    vm with agent

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# az-vm-monitor-logs
-Native monitoring with Azure VMs
+# Azure VM + Monitor Agent
+
+Native monitoring with Azure VMs using the Azure Monitor Agent.
+
+## Requirements
+
+For the Azure Monitor Agent, check the [requirements][1].
+
+- Permissions
+- Authentication / Identity
+- Networking / Azure Firewall
+
+
+
+[1]: https://learn.microsoft.com/en-us/azure/azure-monitor/agents/azure-monitor-agent-manage?tabs=azure-portal
---
+++ infra/main.tf
@@ -0,0 +1,132 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+  backend "local" {
+    path = ".workspace/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+### Group ###
+resource "azurerm_resource_group" "default" {
+  name     = "rg-${var.workload}"
+  location = var.location
+}
+
+### Network ###
+
+resource "azurerm_virtual_network" "default" {
+  name                = "vnet-${var.workload}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = "subnet-default"
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+### Virtual Machine ###
+
+resource "azurerm_public_ip" "main" {
+  name                = "pip-${var.workload}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "nic-${var.workload}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+
+  ip_configuration {
+    name                          = "default"
+    subnet_id                     = azurerm_subnet.default.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+locals {
+  admin_username = "azureuser"
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                  = "vm-${var.workload}"
+  location              = azurerm_resource_group.default.location
+  resource_group_name   = azurerm_resource_group.default.name
+  size                  = var.vm_size
+  admin_username        = local.admin_username
+  admin_password        = "P@ssw0rd.123"
+  network_interface_ids = [azurerm_network_interface.main.id]
+
+  custom_data = filebase64("${path.module}/cloud-init.sh")
+
+  // Monitor Agent
+  // 
+  identity {
+    type = "SystemAssigned"
+  }
+
+  admin_ssh_key {
+    username   = local.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    name                 = "osdisk-${var.workload}"
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = var.vm_image_offer
+    sku       = var.vm_image_sku
+    version   = var.vm_image_version
+  }
+
+  lifecycle {
+    ignore_changes = [
+      custom_data
+    ]
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "azure_monitor_agent" {
+  name                       = "monitor-agent"
+  virtual_machine_id         = azurerm_linux_virtual_machine.main.id
+  publisher                  = "Microsoft.Azure.Monitor"
+  type                       = "AzureMonitorWindowsAgent"
+  type_handler_version       = "1.27.4"
+  auto_upgrade_minor_version = true
+  automatic_upgrade_enabled  = true
+}
+
+### Log Analytics ###
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "log-${var.workload}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
