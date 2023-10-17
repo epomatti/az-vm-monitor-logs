8bb2d28a70f5cb0989d89f1e5f08be085e623c3e
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-10-17 20:27:14 -0300

    sentinel

--- infra4/main.tf
+++ infra4/main.tf
@@ -29,6 +29,18 @@ module "vm" {
   size      = var.vm_size
 }
 
+resource "azurerm_log_analytics_workspace" "default" {
+  name                = "log-${var.workload}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_sentinel_log_analytics_workspace_onboarding" "default" {
+  workspace_id = azurerm_log_analytics_workspace.default.id
+}
+
 module "extension" {
   count  = var.monitor_agent_enabled == true ? 1 : 0
   source = "./modules/extension"
